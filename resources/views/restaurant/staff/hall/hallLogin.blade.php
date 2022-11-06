b5c88d75f86d6cb7989c5f095e0d0aaca96cf5ad
yukke1116 <106470836+yukke1116@users.noreply.github.com>
2022-11-06 17:46:21 +0900

    Add files via upload

---
+++ resources/views/restaurant/staff/hall/hallLogin.blade.php
@@ -0,0 +1,17 @@
+@extends('restaurant.layout.layoutStaff')
+@section('content')
+<div class="content">
+    <h1>ログイン完了</h1>
+    <form action="hallTop" method="post">
+    @csrf
+        <p>{{ $name_or_position }}でログイン完了しました。</p>
+        <label for="restaurant_name" class="none"></label>
+        <input type="text" name="restaurant_name" id="restaurant_name" value="@php echo $restaurant_name; @endphp" class="none">
+        <label for="name_or_position" class="none"></label>
+        <input type="text" name="name_or_position" id="name_or_position" value="@php echo $name_or_position; @endphp" class="none">
+        <div class="buttonMenu">
+            <button type="submit">トップ画面へ</button>
+        </div>
+    </form>
+</div>
+@endsection
